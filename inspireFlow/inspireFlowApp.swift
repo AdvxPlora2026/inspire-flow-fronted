c64f2b79f2c2be63f88aa067385bcdab6afe0a60
WenfengYe <wenfeng110402@icloud.com>
2026-07-23 14:01:28 +0800

    Initial Commit

---
+++ inspireFlow/inspireFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  inspireFlowApp.swift
+//  inspireFlow
+//
+//  Created by 叶文峰 on 2026/7/23.
+//
+
+import SwiftUI
+
+@main
+struct inspireFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
